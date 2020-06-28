eeffe99397c71b2e6fc9249b7f90249118b2acef
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-06-28 19:23:06 -0400

    added the call to api along with user input

--- src/components/ZipContainer.jsx
+++ src/components/ZipContainer.jsx
@@ -2,12 +2,45 @@ import React, { Component } from 'react';
 
 
 class zipContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Allplaces:[],//will hold the addresses returned
+            Zip: "",//holds the zip that the user enters
+            Exist: false//sees if state exists
+        }
+    }
+
+    onChange= (event) =>{//sets the Zip state to that of the one user entered
+        this.setState({Zip : event.target.value})
+    }
+
+    fetchLocations= async() =>{ //calls api after the zip state is updated this way it can get information for specific zip address
+        try{
+            let zipGiven = this.Zip;
+            console.log('this is the zip: '+zipGiven);
+            let response = await fetch('http://ctp-zip-api.herokuapp.com/zip/'+zipGiven, { method: 'GET'} )
+            console.log(response);
+            const status = response.status
+            let result = await response.json();
+            if (status === 400 || status === 500) {
+                console.log(result.error)
+              } else {
+                this.setState({Allplaces:result.data, Exist:true})//updates array and makes it so if the address is real Exist is true. 
+                console.log('array of state is updated')
+            }
+
+        }catch (error){
+            console.log(error);
+        }
+    }
 
     render(){
+        const{Zip}=this.state
         return(
             <div>
-                <form OnSubmit={this.OnSubmit}>
-                    <input type="text" onChange={this.onChange}></input>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" onChange={this.onChange} value={Zip}></input>
                     <input type="submit"></input>
                 </form>
             </div>
