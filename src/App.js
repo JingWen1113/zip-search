a1a43937f49a5e71b5053b0fd056728418d9166b
Jing Wen <wenroujing@hotmail.com>
2020-07-08 00:41:04 -0400

    adding heading style completed close # 7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
-import ZipContainer from './components/ZipContainer';
+import ZipContainer from "./components/ZipContainer";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-        Allplaces:[],
-        Zip: ""
-    }
-}
-  render(){
-  return (
-    <div className="App">
-        <h1>Zip Code Search</h1>
-          <ZipContainer/>
-    </div>
-  );
+      Allplaces: [],
+      Zip: "",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="heading">Zip Code Search</h1>
+        <h3 className="second-heading">Welcome to the city!</h3>
+        <ZipContainer />
+      </div>
+    );
   }
 }
 
